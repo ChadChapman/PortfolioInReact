4416c0d26fddc149e0446ddf09139c6621cdbf7e
git_dave <ctchapman@gmail.com>
2018-09-05 00:44:33 -0700

    added education dialog

--- src/components/AutoGrid.js
+++ src/components/AutoGrid.js
@@ -3,11 +3,12 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import MenuAppBar from './MenuAppBar'
 import FooterBar from './FooterBar'
 import TechnologyExpansionPanel from './TechnologyExpansionPanel'
 import ProjectsPanel from './ProjectsPanel'
 import BufferBar from './BufferBar'
+import BlogDialog from './dialogs/BlogDialog'
+import EducationDialog from './dialogs/EducationDialog'
 
 const styles = theme => ({
     root: {
@@ -26,17 +27,12 @@ const styles = theme => ({
         color: theme.palette.text.title,
         // marginBottom: theme.spacing.unit * 2,
     },
-    stickyNodes: {
-      position: 'fixed'
-    },
 });
 
 function AutoGrid(props) {
     const { classes } = props;
-
     return (
         <div className={classes.root}>
-
             <BufferBar />
             <Grid container spacing={24}>
                 <Grid item xs={12} sm={3}>
@@ -74,25 +70,31 @@ function AutoGrid(props) {
                 <Grid item xs={12} sm={6}>
                     {/*<Paper className={classes.paper}>Current Projects</Paper>*/}
                     {/*<ProjectsPanel />*/}
-                    <Paper className={classes.paper}>Blog</Paper>
+                    {/*<Paper className={classes.paper}>Blog</Paper>*/}
+                    <Paper className={classes.paper}>
+                        <BlogDialog />
+                    </Paper>
                 </Grid>
                 <Grid item xs={12} sm={3}>
                     {/*<Paper className={classes.paper}>Blog</Paper>*/}
                 </Grid>
             </Grid>
             <Grid container spacing={24}>
-                <Grid item xs={6} sm={3}>
-                    <Paper className={classes.paper}>Background</Paper>
+                <Grid item xs={6} sm={4}>
+                    <Paper className={classes.paper}>Personal Background</Paper>
                 </Grid>
-                <Grid item xs={6} sm={3}>
+                <Grid item xs={6} sm={4}>
                     <Paper className={classes.paper}>Professional History</Paper>
                 </Grid>
-                <Grid item xs={6} sm={3}>
-                    <Paper className={classes.paper}>Education</Paper>
+                <Grid item xs={12} sm={4}>
+                    {/*<Paper className={classes.paper}>Education</Paper>*/}
+                    <Paper className={classes.paper}>
+                        <EducationDialog />
+                    </Paper>
                 </Grid>
-                <Grid item xs={6} sm={3}>
-                    <Paper className={classes.paper}>Mentoring</Paper>
-                </Grid>
+                {/*<Grid item xs={6} sm={3}>*/}
+                    {/*<Paper className={classes.paper}>Mentoring</Paper>*/}
+                {/*</Grid>*/}
             </Grid>
             <Grid container spacing={24}>
                 <Grid item xs={12} sm={6}>
--- src/components/dialogs/EducationDialog.js
+++ src/components/dialogs/EducationDialog.js
@@ -3,37 +3,20 @@ import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
 import Slide from '@material-ui/core/Slide';
-import BlogInfoCard from './infocards/BlogInfoCard'
-import Typography from '@material-ui/core/Typography'
+import EduInfoCard from './infocards/EduInfoCard'
 
 function Transition(props) {
     return <Slide direction="up" {...props} />;
 }
 
-// function Transition(props) {
-//     return <BlogInfoCard />
-// }
-
 const styles = theme => ({
     root: {
         flexGrow: 1,
     },
-    paperStyle: {
-        //padding: theme.spacing.unit * 2,
-        //textAlign: 'center',
-        // color: theme.palette.text.secondary,
-        //color: theme.palette.text.title,
-        //font
-        // marginBottom: theme.spacing.unit * 2,
-        case: "sentence",
-
-    },
 });
 
-class BlogDialog extends React.Component {
+class EducationDialog extends React.Component {
     state = {
         open: false,
     };
@@ -51,7 +34,7 @@ class BlogDialog extends React.Component {
             <div>
                 <Button onClick={this.handleClickOpen}
                         fullWidth="true">
-                    Blog
+                    Education
                 </Button>
                 <Dialog
                     open={this.state.open}
@@ -61,21 +44,18 @@ class BlogDialog extends React.Component {
                     aria-labelledby="alert-dialog-slide-title"
                     aria-describedby="alert-dialog-slide-description"
                 >
-                    <DialogTitle id="alert-dialog-slide-title">
-                        {"Where's your blog, Chad?"}
-                    </DialogTitle>
+                    {/*<DialogTitle id="alert-dialog-slide-title">*/}
+                        {/*{"Where's your blog, Chad?"}*/}
+                    {/*</DialogTitle>*/}
                     <DialogContent>
                         {/*<DialogContentText id="alert-dialog-slide-description">*/}
-                            {/*<BlogInfoCard />                            */}
+                        {/*<BlogInfoCard />                            */}
                         {/*</DialogContentText>*/}
-                        <BlogInfoCard />
+                        <EduInfoCard />
                     </DialogContent>
                     <DialogActions>
-                        <Button onClick={this.handleClose} color="primary">
-                            nah, I'm good
-                        </Button>
-                        <Button href="http://www.codecrab.me/" color="primary">
-                            to the old blog!
+                        <Button onClick={this.handleClose} color="inherit">
+                            GO HUSKIES!
                         </Button>
                     </DialogActions>
                 </Dialog>
@@ -84,4 +64,4 @@ class BlogDialog extends React.Component {
     }
 }
 
-export default BlogDialog;
+export default EducationDialog;
