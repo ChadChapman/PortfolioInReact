2bf64958a022e725322f448ef08fb26e1ab47374
git_dave <ctchapman@gmail.com>
2018-09-05 12:50:16 -0700

    code tacoma info card done

---
+++ src/components/dialogs/CodeTacomaDialog.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+import BlogInfoCard from './infocards/BlogInfoCard'
+import Typography from '@material-ui/core/Typography'
+
+function Transition(props) {
+    return <Slide direction="up" {...props} />;
+}
+
+// function Transition(props) {
+//     return <BlogInfoCard />
+// }
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    paperStyle: {
+        //padding: theme.spacing.unit * 2,
+        //textAlign: 'center',
+        // color: theme.palette.text.secondary,
+        //color: theme.palette.text.title,
+        //font
+        // marginBottom: theme.spacing.unit * 2,
+        case: "sentence",
+
+    },
+});
+
+class BlogDialog extends React.Component {
+    state = {
+        open: false,
+    };
+
+    handleClickOpen = () => {
+        this.setState({ open: true });
+    };
+
+    handleClose = () => {
+        this.setState({ open: false });
+    };
+
+    render() {
+        return (
+            <div>
+                <Button onClick={this.handleClickOpen}
+                        fullWidth="true">
+                    Blog
+                </Button>
+                <Dialog
+                    open={this.state.open}
+                    TransitionComponent={Transition}
+                    keepMounted
+                    onClose={this.handleClose}
+                    aria-labelledby="alert-dialog-slide-title"
+                    aria-describedby="alert-dialog-slide-description"
+                >
+                    <DialogTitle id="alert-dialog-slide-title">
+                        {"Where's your blog, Chad?"}
+                    </DialogTitle>
+                    <DialogContent>
+                        {/*<DialogContentText id="alert-dialog-slide-description">*/}
+                        {/*<BlogInfoCard />                            */}
+                        {/*</DialogContentText>*/}
+                        <BlogInfoCard />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.handleClose} color="primary">
+                            nah, I'm good
+                        </Button>
+                        <Button href="http://www.codecrab.me/" color="primary">
+                            to the old blog!
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+        );
+    }
+}
+
+export default BlogDialog;
---
+++ src/components/dialogs/infocards/INAInfoCard.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+    card: {
+        //minWidth: 275,
+        flexGrow: 1,
+    },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+    },
+    title: {
+        marginBottom: 16,
+        fontSize: 14,
+    },
+    pos: {
+        marginBottom: 12,
+    },
+};
+
+function BlogInfoCard(props) {
+    const { classes } = props;
+    const bull = <span className={classes.bullet}>•</span>;
+
+    return (
+        <Card className={classes.card}>
+            <CardContent>
+                <Typography className={classes.title} color="textSecondary">
+                    Whoops, I haven't migrated my blog over to this site yet!
+                </Typography>
+                {/*<Typography variant="headline" component="h2">*/}
+                <Typography className={classes.title} color="textSecondary">
+                    <p>warning: the older stuff is  a bit cringy</p>
+                    <p>{bull} {bull} {bull} {bull} {bull} {bull}</p>
+                    <p>OK, you were warned</p>
+                </Typography>
+            </CardContent>
+            {/*<CardActions>*/}
+            {/*<Button href="http://www.codecrab.me/" size="small">old blog</Button>*/}
+            {/*</CardActions>*/}
+        </Card>
+    );
+}
+
+BlogInfoCard.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(BlogInfoCard);
